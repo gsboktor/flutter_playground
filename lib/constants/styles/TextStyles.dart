a1e4fe32b5054ee60caa2973455f636cb78d90b4
George Boktor <gsboktor@gmail.com>
2021-10-17 11:51:45 -0500

    fonts and project hierarchy

---
+++ lib/constants/styles/TextStyles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const TextStyle HeaderTextStyle = TextStyle(
+    fontFamily: 'Garamond', fontSize: 36.0, fontWeight: FontWeight.w600);
+
+const TextStyle MainTextStyle = TextStyle(
+    fontFamily: 'Garamond', fontSize: 18.0, fontWeight: FontWeight.w600);
+
+const TextStyle UserHeaderTextStyle = TextStyle(
+    fontFamily: 'Futura', fontSize: 14.0, fontWeight: FontWeight.w500);
+
+const TextStyle UserSubHeaderTextStyle = TextStyle(
+    fontFamily: 'Futura', fontSize: 12.0, fontWeight: FontWeight.w500);
+
+const TextStyle TagTextStyle = TextStyle(
+    fontFamily: 'Futura',
+    fontSize: 12.0,
+    fontWeight: FontWeight.w500,
+    color: Colors.white);
+
+const TextStyle DateAndMiscTextStyle = TextStyle(
+    fontFamily: 'Futura',
+    fontSize: 12.0,
+    fontWeight: FontWeight.w500,
+    color: Color.fromARGB(255, 196, 196, 196));
